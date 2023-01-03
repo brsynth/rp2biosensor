29644c3cd703344150972fa4d6a194565961950a
Thomas Duigou <thomas.duigou@inrae.fr>
2023-01-03 13:44:55 +0100

    test(test_main.py): fix mismatch due to SVG tags

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,6 +8,35 @@ SINK_PATH = Path(__file__).resolve().parent / 'data' / 'input' / 'sink-h2o2.csv'
 OUTPUT_DIR_PATH = Path(__file__).resolve().parent / 'data' / 'output_dir'
 OUTPUT_FILE_PATH = Path(__file__).resolve().parent / 'data' / 'output_file'
 
+# Compare except with expected file except for SVGs
+def filter_lines(line):
+    """Filter unwanted lines
+    
+    This method is used when filtering lines with the filter method.
+    """
+    if line.lstrip().startswith('"svg": '):
+        return False
+    return True
+
+def compare_files(ref_dir: Path, test_dir: Path, files_to_cmp: list):
+    """Compare two set of files
+    
+    Files are compared 2 by 2, coming from 2 different directories
+    """
+    match = []
+    mismatch = []
+    for file in files_to_cmp:
+        with open(ref_dir / file) as ref, \
+             open(test_dir / file) as test:
+            f1 = filter(filter_lines, ref)
+            f2 = filter(filter_lines, test)
+            if all(x == y for x,y in zip(f1, f2)):
+                match.append(file)
+            else:
+                mismatch.append(file)
+    return match, mismatch
+
+
 def test_dir_output(tmpdir):
     temp_path = tmpdir / 'dir_case'  # tmpdir scope is session wised
     options = {
@@ -25,13 +54,18 @@ def test_dir_output(tmpdir):
     ]
     args = SimpleNamespace(**options)
     run(args)
-    match, mismatch, errors = cmpfiles(OUTPUT_DIR_PATH, temp_path, files_to_cmp)
+
+    match, mismatch = compare_files(
+        ref_dir=OUTPUT_DIR_PATH,
+        test_dir=temp_path,
+        files_to_cmp=files_to_cmp
+        )
+    
     try:
-        assert all(item in match  for item in files_to_cmp)
+        assert all(item in match for item in files_to_cmp)
     except AssertionError as e:
         print("Matched Files    : {}".format(match))
         print("Mismatched Files : {}".format(mismatch))
-        print("Errors           : {}".format(errors))
         raise e
 
 def test_file_output(tmpdir):
@@ -46,11 +80,11 @@ def test_file_output(tmpdir):
     files_to_cmp = ['biosensor.html']
     args = SimpleNamespace(**options)
     run(args)
-    match, mismatch, errors = cmpfiles(OUTPUT_FILE_PATH, temp_path, files_to_cmp)
+
+    match, mismatch = compare_files(ref_dir=OUTPUT_FILE_PATH, test_dir=temp_path, files_to_cmp=files_to_cmp)
     try:
         assert 'biosensor.html' in match
     except AssertionError as e:
         print("Matched Files    : {}".format(match))
         print("Mismatched Files : {}".format(mismatch))
-        print("Errors           : {}".format(errors))
         raise e
